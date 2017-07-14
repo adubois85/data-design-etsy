aaecef76193bf851cde2ea3e9eb8f69345e02db7
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-14 09:37:24 -0600

    Building persona

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
 		<title>A Lesson in Data Design</title>
 	</head>
 	<body>
+		<h1>Persona</h1>
+			<p>
+				Elaine Atwood is a 21 year-old computer science undergraduate at the University of California, Los
+				Angeles.  While she doesn't have as much time these days to devote to it, since she was a little girl,
+				she has been making jewelry as gifts for friends and family.  Both to keep in practice and to bring in a
+				little extra money for those long nights in the coffee shop finishing projects, she wants a quick and
+				simple way to sell what she creates.
+			</p>
+			<p>
 
+			</p>
 	</body>
 </html>

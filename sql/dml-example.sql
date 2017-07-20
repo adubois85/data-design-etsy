df96e09de0223be0849f28f52c29b66f5c686c32
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-20 03:20:19 -0600

    most DDL scripts entered

---
+++ sql/dml-example.sql
@@ -0,0 +1,30 @@
+SELECT listingId, listingProfileId, listingContent
+FROM listing
+WHERE listingNumberOfItems = 1;
+
+INSERT INTO listing(profileId, profileAtHandle, profileEmail)
+VALUES(DEFAULT, "adoobie", "adubois85@gmail.com");
+
+UPDATE profile SET profileEmail = "adubois@alumni.uci.edu"
+WHERE profileEmail LIKE "adubois%";
+
+DELETE FROM listing WHERE listingDate > NOW();
+
+UPDATE profile SET profileAtHandle = "adubois85"
+WHERE profileAtHandle = "adoobie";
+
+SELECT listingId, listingProfileId, listingContent
+FROM listing
+WHERE listingContent LIKE "%cool stuff%";
+
+DELETE FROM profile WHERE profileEmail LIKE "%yahoo.com";
+
+SELECT listingContent
+FROM listing
+WHERE LENGTH(listingContent) <100;
+
+DELETE FROM listing WHERE SUM(boughtDate - listingDate) < 100000;
+
+SELECT listingId, listingProfileId, listingContent
+FROM listing
+WHERE listingContent LIKE "%cool stuff%" AND ;

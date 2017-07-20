bf0ffd046cf793488159db61e818a0e5565c39c4
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-20 15:23:55 -0600

    Filled out rest of DDL scripts, all tables accounted for

--- sql/data-design-etsy.sql
+++ sql/data-design-etsy.sql
@@ -10,13 +10,32 @@ CREATE TABLE profile (
 	PRIMARY KEY(profileId)
 );
 
-CREAT TABLE listing (
+CREATE TABLE listing (
 	listingId INT 	UNSIGNED AUTO_INCREMENT NOT NULL,
 	listingProfileId INT UNSIGNED NOT NULL,
-	listingContent VARCHAR NOT NULL,
+	listingContent VARCHAR(5000) NOT NULL,
 	listingDate DATETIME(6) NOT NULL,
 	listingNumberOfItems INT UNSIGNED NOT NULL,
-	FOREIGN KEY listingProfileId REFERENCES profile(profileId),
-	PRIMARY KEY listingId
-)
+	FOREIGN KEY(listingProfileId) REFERENCES profile(profileId),
+	PRIMARY KEY (listingId)
+);
 
+CREATE TABLE photos (
+	photoListingId INT UNSIGNED NOT NULL,
+	photoProfileId INT UNSIGNED NOT NULL,
+	photoDate DATETIME(6) NOT NULL,
+	FOREIGN KEY(photoListingId) REFERENCES listing(listingId),
+	FOREIGN KEY(photoProfileId) REFERENCES profile(profileId),
+	PRIMARY KEY (photoListingId, photoProfileId)
+);
+
+CREATE TABLE bought (
+	boughtListingId INT UNSIGNED NOT NULL,
+	boughtBuyerId INT UNSIGNED NOT NULL,
+	boughtSellerId INT UNSIGNED NOT NULL,
+	boughtDate DATETIME(6) NOT NULL,
+	FOREIGN KEY(boughtListingId) REFERENCES listing(listingId),
+	FOREIGN KEY(boughtBuyerId) REFERENCES profile(profileId),
+	FOREIGN KEY(boughtSellerId) REFERENCES profile(profileId),
+	PRIMARY KEY(boughtListingId, boughtBuyerId, boughtSellerId)
+);
